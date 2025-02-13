4770995fb758764298dff9620c672d77b2b4065e
Mayel de Borniol <git@mayel.space>
2025-02-13 22:26:40 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/978

---
+++ lib/mix/install.ex
@@ -0,0 +1,30 @@
+defmodule Mix.Tasks.Upcycle.Install do
+  use Igniter.Mix.Task
+  alias Bonfire.Common.Mix.Tasks.Helpers
+
+  @shortdoc "Install an extension into the parent app"
+  @doc """
+  Usage:
+  `just mix social.install`
+  """
+
+  @app :upcycle
+
+  def igniter(igniter, args) do
+    # IO.inspect(args, label: "Args")
+
+    app_dir = Application.app_dir(@app)
+
+    igniter
+    # first we install Ember since this flavour includes that one
+    |> Igniter.compose_task(Mix.Tasks.Ember.Install, [])
+    # then we run custom tasks for this flavour
+    |> Helpers.igniter_copy(Path.join(app_dir, "priv/templates/lib/"), "lib/")
+    |> Helpers.igniter_copy(
+      Path.wildcard(Path.join(app_dir, "deps.*")),
+      "config/current_flavour/"
+    )
+    # finally we run the standard installer for this flavour (which includes copying config and migrations)
+    |> Igniter.compose_task(Mix.Tasks.Bonfire.Extension.Installer, [@app])
+  end
+end
