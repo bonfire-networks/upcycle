363d8d2ee8f3cff32f0793210ebd18776d62074e
Mayel de Borniol <git@mayel.space>
2025-06-13 13:50:17 +0100

    igniter

--- lib/mix/install.ex
+++ lib/mix/install.ex
@@ -10,9 +10,7 @@ defmodule Mix.Tasks.Upcycle.Install do
 
   @app :upcycle
 
-  def igniter(igniter, args) do
-    # IO.inspect(args, label: "Args")
-
+  def igniter(igniter) do
     app_dir = Application.app_dir(@app)
 
     igniter
